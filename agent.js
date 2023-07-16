c28d350d503d0acdfb4f50475ac814161bb740ac
tubleronchik <m.bystramovich@gmail.com>
2023-07-16 21:40:22 +0300

    add queue

--- agent.js
+++ agent.js
@@ -10,15 +10,21 @@ web3.eth.accounts.wallet.add(config.spot_pk)
 
 const ipfs = create("http://127.0.0.1:5001")
 
+const AVAILABLE = "AVAILABLE"
+const NOT_AVAILABLE = "NOT AVAILABLE"
+
 class Agent {
     constructor() {
         this.demand = {}
         this.offer = {}
+        this.demandQueue = []
         this.liabilityAddress = ""
+        this.STATUS = "AVAILABLE"
         this.readABI()
-        this.onProviderMsg = this.onProviderMsg.bind(this); // to send context of the Provider into function
+        this.onProviderMsg = this.onProviderMsg.bind(this); // to send the context of the Provider into the function
         this.onSpotMsg = this.onSpotMsg.bind(this);
         this.createOffer = this.createOffer.bind(this);
+        this.manageQueue = this.manageQueue.bind(this);
         this.getObjectiveFromLiability = this.getObjectiveFromLiability.bind(this);
         this.ipfsSubscribe()
         
@@ -35,19 +41,30 @@ class Agent {
     async onProviderMsg(msg) {
         if (msg.from == config.ipfs_id_dapp) {
             let stringMsg = String.fromCharCode(...Array.from(msg.data))
-            let jsonMsg = JSON.parse(stringMsg) 
-            stringMsg = String.fromCharCode(...Array.from(msg.data))
             this.demand = JSON.parse(stringMsg) 
-            this.offer = await this.createOffer()
-            console.log("Offer:")
-            console.log(this.offer)
-            await this.sendPubsubMsg(this.offer, config.provider_ipfs_topic)
+            console.log("Queue")
+            console.log(this.demandQueue)
+
+            if (this.STATUS == AVAILABLE) {
+
+                this.STATUS = NOT_AVAILABLE
+                this.offer = await this.createOffer()
+                console.log("Offer:")
+                console.log(this.offer)
+                await this.sendPubsubMsg(this.offer, config.provider_ipfs_topic)
+            }
+            else {
+                this.manageQueue()
+            }
 
         }
         else if (msg.from == config.ipfs_id_provider) {
+
             let stringMsg = String.fromCharCode(...Array.from(msg.data))
             let jsonMsg = JSON.parse(stringMsg) 
+
             if (jsonMsg.liability) {
+
                 this.liabilityAddress = jsonMsg.liability
                 console.log(`Liability address: ${this.liabilityAddress}`)
                 const objective = await this.getObjectiveFromLiability()
@@ -55,6 +72,20 @@ class Agent {
                 await this.sendPubsubMsg(objectiveMsg, config.spot_ipfs_topic)
                 
             }
+        }
+    }
+
+    async manageQueue() {
+        if (this.demand && (this.STATUS == NOT_AVAILABLE)) {
+            this.demandQueue.push(this.demand)
+        }
+        else {
+            this.STATUS = NOT_AVAILABLE
+            this.demand = this.demandQueue.shift()
+            this.offer = await this.createOffer()
+            console.log("Offer from the queue:")
+            console.log(this.offer)
+            await this.sendPubsubMsg(this.offer, config.provider_ipfs_topic)
         }
     }
 
@@ -68,6 +99,11 @@ class Agent {
                 console.log(`New result from Spot: ${result}`)
                 const resultMsg = {"result": result}
                 await this.sendPubsubMsg(resultMsg, config.provider_ipfs_topic)
+                this.STATUS = "AVAILABLE"
+                this.demand = undefined
+                if (this.demandQueue.length > 0) {
+                    this.manageQueue()
+                }
             }
             
         }
@@ -114,7 +150,7 @@ class Agent {
             { t: 'uint256', v: offer.nonce },
             { t: 'address', v: offer.sender }
         );
-        offer.signature = await web3.eth.accounts.sign(hash, config.spot_pk);
+        offer.signature = await web3.eth.accounts.sign(hash, config.spot_pk)
         return offer;
     }
 
